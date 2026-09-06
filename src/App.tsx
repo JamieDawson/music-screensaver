d1b2ab64a182e2d41a7c8bae2fa05b15d16933ea
JamieDawson <jamierobertdawson@gmail.com>
2026-09-05 23:58:23 -0700

    Feat: Make divs appear on screen when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import { animated } from "react-spring";
+
 function App() {
+  type Spawn = {
+    x: number;
+    y: number;
+  };
+
+  const [spawns, setSpawns] = useState<Spawn[]>([]);
+
+  const Spawn = ({ x, y }: Spawn) => {
+    const spawnStyles = {
+      position: "absolute",
+      left: x + "px",
+      top: y + "px",
+      transform: `translate(-50%, -50%)`,
+    };
+
+    return (
+      <animated.div style={spawnStyles}>
+        <p>+1</p>
+      </animated.div>
+    );
+  };
+
+  const clickHandler = (event: React.MouseEvent) => {
+    const { clientX, clientY } = event;
+    console.log(clientX, clientY);
+
+    setSpawns([
+      ...spawns,
+      {
+        x: clientX,
+        y: clientY,
+      },
+    ]);
+  };
+
   return (
     <div className="container">
       <div>Music screen saver</div>
-      <div className="rectangle">
+      <div className="rectangle" onClick={(event) => clickHandler(event)}>
+        {spawns.map((spawn) => {
+          return <Spawn x={spawn.x} y={spawn.y} />;
+        })}
         <div className="iconContainer">
           <div className="icon">icon</div>
         </div>
