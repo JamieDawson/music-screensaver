aba2f9535c4e2a3c3e43822466dec1ceffeda7f5
JamieDawson <jamierobertdawson@gmail.com>
2026-09-05 21:47:34 -0700

    Feat: Start project by creating background for div

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <div>Music screen saver</div>
+
+      <div className="rectangle"></div>
+    </div>
+  );
+}
+
+export default App;
