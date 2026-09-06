21dd093401831f8f0de9bb8fbd44d26492dda8be
JamieDawson <jamierobertdawson@gmail.com>
2026-09-05 22:41:45 -0700

    Feat: Add screen background and icon with updated for centering

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ function App() {
   return (
     <div className="container">
       <div>Music screen saver</div>
-
-      <div className="rectangle"></div>
+      <div className="rectangle">
+        <div className="iconContainer">
+          <div className="icon">icon</div>
+        </div>
+      </div>
     </div>
   );
 }
